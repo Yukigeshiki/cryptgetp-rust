1b592e45441acdfc7b9714e638ab8071c82d44df
Laurence <laurence@robothouse.io>
2023-09-26 15:23:43 +0200

    update clippy pedantic attributes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 #![warn(clippy::pedantic)]
-#![allow(
-    clippy::unused_async,
-    clippy::missing_panics_doc,
-    clippy::missing_errors_doc
-)]
 
 use clap::{Parser, Subcommand};
 use colored::Colorize;
